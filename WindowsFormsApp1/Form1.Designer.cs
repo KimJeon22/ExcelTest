a72aa425dc452f0cf659a3174aa2264e64f8081a
KimJeon22 <rlawlgus5711@gmail.com>
2019-10-06 19:43:04 +0900

    엑셀 데이터 읽기 및 데이터 검색

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.searchText = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(182, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "수집";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(292, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(296, 359);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // searchText
+            // 
+            this.searchText.Font = new System.Drawing.Font("굴림", 15F);
+            this.searchText.Location = new System.Drawing.Point(12, 65);
+            this.searchText.Name = "searchText";
+            this.searchText.Size = new System.Drawing.Size(261, 36);
+            this.searchText.TabIndex = 2;
+            this.searchText.TextChanged += new System.EventHandler(this.searchSite);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 127);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 27;
+            this.dataGridView2.Size = new System.Drawing.Size(261, 244);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 382);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.searchText);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.form_load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.DirectoryServices.DirectoryEntry directoryEntry1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox searchText;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Collections.Specialized;
+
+namespace WindowsFormsApp1
+{
+    
+    public partial class Form1 : Form
+    {
+        Excel.Application excelApp = new Excel.ApplicationClass();
+        NameValueCollection vdata = new NameValueCollection();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = "C:/test/playauto_emp_site_code_23.xls";
+            try
+            {
+                Excel.Workbook wb = excelApp.Workbooks.Open(url);
+                Excel.Sheets _Sheets = wb.Sheets;
+                //첫번째 worksheet 선택
+                Excel.Worksheet ws = _Sheets[1] as Excel.Worksheet;
+
+                object chkcell1 = ws.Range["A1"].Value;
+
+                if (chkcell1.ToString().Equals("쇼핑몰코드표"))
+                {
+                    //열들에 data를 배열로 받아옴
+                    foreach (Excel.Range _row in ws.UsedRange.Rows)
+                    {
+                        Object[,] edata = (System.Object[,])_row.Value;
+                        if (edata[1, 2] != null)
+                        {
+                            vdata.Add((string)edata[1, 1], (string)edata[1, 2]);
+                        }
+                    }
+                    SetupGridView(vdata);
+                }
+                else
+                {
+                    throw new Exception("양식 오류입니다.");
+                }
+
+                wb.Close();
+                ws = null;
+                _Sheets = null;
+            }
+            catch (Exception ee)
+            {
+                throw new Exception(ee.Message); 
+            }
+
+        }
+
+        private void SetupGridView(NameValueCollection vdata)
+        {
+            foreach (string fieldName in vdata.Keys)
+            {
+                foreach (string fieldValue in vdata.GetValues(fieldName))
+                {
+                    if (!(fieldValue == ""))
+                    { 
+                        string[] row = { fieldName, fieldValue };
+                        dataGridView1.Rows.Add(row);
+                    }
+                }
+            }
+        }
+
+        private void SetupGridView(String searchKey)
+        {
+                var searchList = from key in vdata.Keys.Cast<String>()
+                                 from value in vdata.GetValues(key)
+                                 where key.Contains(searchKey)
+                                 select new { key, value };
+
+                foreach (var test in searchList)
+                {
+                    string[] row = { test.key, test.value };
+                    dataGridView2.Rows.Add(row);
+                }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void searchSite(object sender, EventArgs e)
+        {
+              if (dataGridView2.Rows.Count > 1)
+            {
+                RemoveAllRows();
+            }
+            SetupGridView(searchText.Text);
+        }
+
+        private void RemoveAllRows()
+        {
+            for(int i=0; i<dataGridView2.Rows.Count-1;)
+            {
+                dataGridView2.Rows.Remove(dataGridView2.Rows[i]);
+            }
+        }
+
+        private void form_load(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnCount = 2;
+            dataGridView1.Columns[0].Name = "쇼핑몰";
+            dataGridView1.Columns[1].Name = "코드";
+
+            dataGridView2.ColumnCount = 2;
+            dataGridView2.Columns[0].Name = "쇼핑몰";
+            dataGridView2.Columns[1].Name = "코드";
+        }
+    }
+}
